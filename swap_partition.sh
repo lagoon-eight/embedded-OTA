b734a0c0d7a07c1a67b203d1db67aa09b598857c
Catalin Avram <papiot@gmail.com>
2019-12-29 12:38:44 -0500

    Add install-grub command to write to the correct drive

--- swap_partition.sh
+++ swap_partition.sh
@@ -16,8 +16,14 @@ echo "Modifying grub config..."
 
 sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=$partition_number/" /etc/default/grub
 
-echo "Writing grub..."
+echo "Writing grub to device ..."
+
+sudo grub-install /dev/sdb
+
+echo "Updating grub to this device..."
 
 sudo update-grub
+
+echo "Rebooting..."
 
 reboot
